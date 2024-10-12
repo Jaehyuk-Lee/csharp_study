65d7470c1119a05d92a5cdda36fea0b676643c8c
Jaehyuk-Lee <alex00728@outlook.com>
2024-10-12 21:05:19 +0900

    thread

---
+++ Thread/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ThreadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(Func);
+            myThread.Start();
+        }
+
+        static void Func()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello from thread! " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
