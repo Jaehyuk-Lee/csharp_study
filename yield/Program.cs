76586156f97e21230e2f1b7d4abc286630e57bf6
Jaehyuk-Lee <alex00728@outlook.com>
2024-10-30 09:03:00 +0900

    yield basic

---
+++ yield/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace yield_example
+{
+    class MyEnumerator
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 3)
+                {
+                    yield break;
+                }
+                yield return numbers[i];
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new MyEnumerator();
+            foreach (int i in obj)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
